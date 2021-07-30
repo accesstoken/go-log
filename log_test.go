3a702e2caad4737336b1ca2b1b9f6fca57d088e2
Massoud <massoud77@gmail.com>
2021-07-30 16:55:10 -0700

    added more tests

--- log_test.go
+++ log_test.go
@@ -14,3 +14,23 @@ func TestLogger_Begin(t *testing.T) {
 		t.Errorf("Expected BEGIN received '%s'", output.String())
 	}
 }
+
+func TestLogger_End(t *testing.T) {
+	var output strings.Builder
+	var logger Logger
+	logger.Writer = &output
+	logger.End()
+	if output.String() != "END\n" {
+		t.Errorf("Expected END received '%s'", output.String())
+	}
+}
+
+func TestLogger_Logf(t *testing.T) {
+	var output strings.Builder
+	var logger Logger
+	logger.Writer = &output
+	logger.Logf("Hello World!")
+	if output.String() != "Hello World!\n" {
+		t.Errorf("Expected Hello World! received '%s'", output.String())
+	}
+}
